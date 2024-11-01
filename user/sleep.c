69a128139079cff3146c12a67027480d13974632
belle <huajieyu17@gmail.com>
2024-11-01 21:01:00 +0800

    finished sleep work.

---
+++ user/sleep.c
@@ -0,0 +1,11 @@
+#include "../kernel/types.h"
+#include "user.h"
+
+int main(int argc,char *argv[]){
+  if(argc != 2){
+    fprintf(2,"usage : slepp <time(s)> \n");
+    exit(1);
+  }
+  sleep(atoi(argv[1]));
+  exit(0);
+}
