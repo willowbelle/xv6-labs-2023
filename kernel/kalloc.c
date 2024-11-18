3bcc244aab2211dc5d0067c520da2017b868410a
belle <huajieyu17@gmail.com>
2024-11-18 20:19:17 +0800

    finished lab cow

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -16,71 +16,64 @@ extern char end[]; // first address after kernel.
 // pa to ref array idx
 #define PA2IDX(pa) (((uint64)pa - (uint64)end) / PGSIZE)
 #define PG_REFCNT(pa) (pageref.ref[(uint64)pa / PGSIZE])
-struct run {
+struct run
+{
   struct run *next;
 };
 
-struct {
-  struct spinlock lock;
-  struct run *freelist;
-} kmem;
+struct page
+{
+  int refcnt;
+  // can add other metadata
+};
 
 struct
 {
   struct spinlock lock;
-  int ref[PHYSTOP / PGSIZE];
-} pageref;
+  struct run *freelist;
+  struct page pages[(PHYSTOP - KERNBASE) / PGSIZE];
+} kmem;
 
 void icrRef(void *pa)
 {
-  acquire(&pageref.lock);
-  PG_REFCNT(pa)++;
-  release(&pageref.lock);
-}
-
-void refcnt_init(void *pa)
-{
   if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
-    panic("refcnt_init");
-  acquire(&pageref.lock);
-  PG_REFCNT(pa) = 1;
-  release(&pageref.lock);
+    panic("getRefCnt");
+  int idx = PA2IDX(pa);
+
+  acquire(&kmem.lock);
+  kmem.pages[idx].refcnt++;
+  release(&kmem.lock);
 }
 
 void dcrRef(void *pa)
 {
-  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
-    panic("dcrRef");
-  acquire(&pageref.lock);
-  PG_REFCNT(pa)--;
-  release(&pageref.lock);
+  acquire(&kmem.lock);
+  kmem.pages[PA2IDX(pa)].refcnt--;
+  release(&kmem.lock);
 }
 
 int getRefCnt(void *pa)
 {
   if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("getRefCnt");
-  acquire(&pageref.lock);
-  int ret = PG_REFCNT(pa);
-  release(&pageref.lock);
+  acquire(&kmem.lock);
+  int ret = kmem.pages[PA2IDX(pa)].refcnt;
+  release(&kmem.lock);
   return ret;
 }
 
 void kinit()
 {
   initlock(&kmem.lock, "kmem");
-  initlock(&pageref.lock, "ref_cnt");
-  memset(pageref.ref, 0, sizeof(pageref.ref));
+  memset(kmem.pages, 0, sizeof(kmem.pages));
   freerange(end, (void *)PHYSTOP);
 }
 
-
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -115,20 +108,20 @@ void kfree(void *pa)
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-void *
-kalloc(void)
+void *kalloc(void)
 {
   struct run *r;
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r) {
+  if (r)
     kmem.freelist = r->next;
-    refcnt_init((void*)r);
-  }
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  if (r)
+  {
+    memset((char *)r, 5, PGSIZE); // fill with junk
+    kmem.pages[PA2IDX(r)].refcnt = 1;
+  }
+  return (void *)r;
 }
